05b9ff7fd8fe0dfcd8d076aeda14ba6c08595de8
Dariusz Okonski <darek200180@gmail.com>
2019-04-12 08:54:16 +0100

    Updates

---
+++ precache-manifest.59f7093555ae91b3e052b7edaa858fb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f25d15784e6e2191bfa2",
+    "url": "/react-simple-cinema-app-005/static/js/runtime~main.f1619cb7.js"
+  },
+  {
+    "revision": "870b45e03a3928711e61",
+    "url": "/react-simple-cinema-app-005/static/js/main.0ccb0bc9.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-simple-cinema-app-005/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "870b45e03a3928711e61",
+    "url": "/react-simple-cinema-app-005/static/css/main.e67c171f.chunk.css"
+  },
+  {
+    "revision": "bf0dbd441e27226019351e95d24604d8",
+    "url": "/react-simple-cinema-app-005/index.html"
+  }
+];
