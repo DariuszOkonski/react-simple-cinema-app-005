eec32b08d9344e55ce2423360c5d0a10ba8dea67
Dariusz Okonski <darek200180@gmail.com>
2019-04-12 11:11:06 +0100

    add app.css

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello</h1>
+        <h1>Buy a ticket for a horror of a year</h1>
+
+        <input type="checkbox" id="check" />
+        <label htmlFor="check">I have at least 16 years</label>
+
+        <button>buy ticket</button>
+
+        <h2>You can not watch that movie if you have less then 16 year old!</h2>
+        <h2>You can watch that movie! Enjoy!</h2>
       </div>
     );
   }
